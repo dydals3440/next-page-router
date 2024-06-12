66331dcf514089013a1d0d586aa56efcde9a3890
김용민 <dydals3440@gmail.com>
2024-06-12 22:19:56 +0900

    feat: Initial Setting of PageRouter Project

---
+++ layouts/Layout.jsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+export default function Layout({ children }) {
+	return (
+		<div>
+			<nav>
+				<Link href="/">상품 목록</Link> | <Link href="/cart">장바구니</Link>
+			</nav>
+			<div>{children}</div>
+		</div>
+	);
+}
+
+/**
+ * /login
+ * /main
+ * /home
+ * /products/productsId/1
+ */
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+function ProductPage() {
+	return <div>Hello Next</div>;
+}
+
+export default ProductPage;
+
+// 'localhost:3000/' (index)
+
+/**
+ * 1. 상품 목록 페이지 - '/'
+ * 2. 상품 상세 페이지 - '/products/productId'
+ * 3. 장바구니 페이지 - '/carts'
+ */
