690bde52bbcaeb95126f2d73c642326887739365
김용민 <dydals3440@gmail.com>
2024-07-09 18:14:23 +0900

    chore: script 테스트 변경

--- cypress/e2e/product-page.cy.js
+++ cypress/e2e/product-page.cy.js
@@ -1,7 +1,8 @@
 describe('상품 목록 페이지', () => {
 	// 첫 번째 테스트 시나리오
 	it('페이지에 진입하면 상품 목록이 표시된다. ', () => {
-		cy.visit('http://localhost:3000/');
+		// config 설정을 '/'로 했기에 됨!
+		cy.visit('/');
 		// should: 받아온 엘리먼트가 꼭 뭐가 되야함.
 		// be.visible: cy.attribute를 가진 엘리먼트가 화면에 있을 것 이라고 가정.
 		// cy.get('[data-cy=product-item]').should('be.visible');
